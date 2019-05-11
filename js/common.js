e4471b6b80545b6ec7f3874124abadc8180e67fb
daoke0818 <daoke_li@qq.com>
2019-05-11 18:23:31 +0800

    个人介绍页(含联系方式)以及导航，移动端样式优化，bootstrap升级到4.3.1

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('body>footer').addClass('text-center mainFooter').html(`
+        © 2019 关中刀客在青岛 <a href="http://www.beian.miit.gov.cn" target="_blank"> 鲁ICP备19013610号 </a>
+    <a href="https://github.com/daoke0818/myWeb" target="_blank"><i class="iconfont i-github_bg"></i></a>
+    <a href="https://gitee.com/daoke0818/myWeb" target="_blank"><i class="iconfont i-gitee_bg"></i></a>
+    `)
+});
